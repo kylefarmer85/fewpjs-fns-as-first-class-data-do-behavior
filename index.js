4b946d449bc8837d60b5d29b9f2a679ee4612535
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-26 11:49:34 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let splitTime = time.split(":");
+  let joinTime = splitTime.join('')
+  let newTime = parseInt(joinTime);
+  if (newTime < 1200) {
+    return "Good Morning";
+  } else if (newTime >= 1200 && newTime <= 1700) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
